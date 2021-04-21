4deb22ed4a7ac755e4b25dc6540c3d4b6345527e
Lucas Almeida <lucasecalmeida@gmail.com>
2021-04-21 10:49:08 -0300

    :art: Setting up Modular

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:clinicapp/constants.dart';
 import 'package:clinicapp/screens/dashboard/dashboard_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'app_module.dart';
+
 void main() {
-  runApp(ClinicApp());
+  runApp(
+    ModularApp(
+      module: AppModule(),
+      child: ClinicApp(),
+    ),
+  );
 }
 
 class ClinicApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
       debugShowCheckedModeBanner: false,
       title: 'OdontoPro',
       theme: ThemeData(
@@ -19,6 +28,6 @@ class ClinicApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: DashboardScreen(),
-    );
+    ).modular();
   }
 }
