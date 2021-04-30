c755ba6b88fe6fa277a17291f98b8fc80644b795
Lucas Almeida <lucasecalmeida@gmail.com>
2021-04-30 13:38:08 -0300

    :fire: Simplifying screen structure

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,11 +1,40 @@
 import 'package:flutter/material.dart';
-import 'components/dashboard_screen_body.dart';
+import 'package:clinicapp/constants.dart';
+import 'components/greeting.dart';
+import 'components/upcoming_appointments.dart';
 
 class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: DashboardScreenBody(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Greeting(),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: opSpacing,
+                vertical: opSpacing / 2,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Próximas consultas',
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Nova consulta',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            UpcomingAppointments(),
+          ],
+        ),
+      ),
     );
   }
 }
